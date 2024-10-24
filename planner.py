36e7b9ec09652bdba2a79120df48e9083e520776
nbellsmi <nbellsmi@uwaterloo.ca>
2024-10-24 13:52:35 -0400

    Planner Updates

--- planner.py
+++ planner.py
@@ -1,7 +1,7 @@
 # Type of planner
 POINT_PLANNER=0; TRAJECTORY_PLANNER=1
 
-
+import math
 
 class planner:
     def __init__(self, type_):
@@ -25,7 +25,29 @@ class planner:
 
     # TODO Part 6: Implement the trajectories here
     def trajectory_planner(self):
-        pass
+        TrajectoryType = 0
+        x = 0
+        y = 0
+        Step = 0.1
+        Points = []
+
+        if TrajectoryType == 0:
+            while x < 1.5:
+                y = x**2
+                Point = (x,y)
+                Points.append(Point)
+                x +=Step
+                
+        if TrajectoryType == 1:
+            while x < 2.5:
+                y = 2/(1+math.exp(-2*x))-1
+                Point = (x,y)
+                Points.append(Point)
+                x +=Step
+        
+        return Points
+            
+
         # the return should be a list of trajectory points: [ [x1,y1], ..., [xn,yn]]
         # return 
 
